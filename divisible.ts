12375a48451de01e18670b8e09f89e2fb186a2de
Rameez49 <ulhassanr59@gmail.com>
2023-08-09 21:59:28 +0500

    first commit

---
+++ divisible.ts
@@ -0,0 +1,13 @@
+// Write a program that checks if the given number is  divisible by 3 or 5 or both or not divisible by anyone show output accordingly.
+
+var divisibleNum= 4;
+
+if(divisibleNum%3==0){
+    console.log(`This number is divisible by 3`)
+}
+
+else if(divisibleNum%5==0){
+    console.log(`This number is divisible by 5`)
+}
+
+else (`This number is not divided by 3 and 5 `)
---
+++ temperaturechecker.ts
@@ -0,0 +1,15 @@
+// Write a program that takes temperature and check it. If it is cold then suggest the user to wear warm clothes and so on according to the weather.
+
+var temperature= 45;
+
+if(temperature<0 && temperature>=10){
+    console.log(`Wear warm clothes because temperature is = ${temperature}`)
+}
+else if (temperature>10 && temperature<25){
+    console.log(`Temperature is normal so dont worry`)
+}
+else if(temperature>26){
+    console.log(`Temperature is so hot because temperature is ${temperature}`)
+
+}
+else (`Invalid Temperatur`)
