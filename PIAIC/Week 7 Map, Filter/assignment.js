6e0b6b0a6ac3e5ecf333cadf92aca1a48b1d0f59
Rameez49 <ulhassanr59@gmail.com>
2023-09-03 10:38:24 +0500

    first

---
+++ PIAIC/Week 7 Map, Filter/assignment.js
@@ -0,0 +1,13 @@
+"use strict";
+// 1). Write a program that uses filter to remove all negative numbers from an array of numbers
+// var numbers = [-1, 2, -3, 4, 5];
+// const positiveNumber = numbers.filter((number) => number >= 0);
+// console.log(positiveNumber);
+// function removeNegatives(numbers: number[]): number[] {
+//   // Create a new array to store the non-negative numbers.
+//   const nonNegativeNumbers = numbers.filter((number) => number >= 0);
+//   return nonNegativeNumbers;
+// }
+// const numbers = [-1, 2, -3, 4, 5];
+// const nonNegativeNumbers = removeNegatives(numbers);
+// console.log(nonNegativeNumbers); // [2, 4, 5]
---
+++ PIAIC/Week 7 Map, Filter/assignment.ts
@@ -0,0 +1,14 @@
+// 1). Write a program that uses filter to remove all negative numbers from an array of numbers
+
+// var numbers:number[]=[-1,2,-3,4,5]
+// const positiveNumber = numbers.filter((number) => number >=0)
+
+// console.log(positiveNumber);
+
+// 2). Given an array of numbers [1, 2, 3, 4, 5], use the map method to create a new array where each number is multiplied by 2. 
+
+// const numbers: number[] = [1, 2, 3, 4, 5];
+
+// const doubledNumbers: number[] = numbers.map((num) => num * 2);
+
+// console.log(doubledNumbers);
