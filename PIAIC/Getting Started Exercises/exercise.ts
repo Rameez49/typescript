67610e09b1366242a297f04c0df4637da3279db3
Rameez49 <ulhassanr59@gmail.com>
2023-09-05 15:59:43 +0500

    first

---
+++ PIAIC/Getting Started Exercises/exercise.ts
@@ -0,0 +1,133 @@
+// Qno 1.) Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+
+// var stotreName:String = "Rameez Ul Hassan"
+
+// console.log(`"Hello ${stotreName}, would you like to learn some python today?"`);
+
+// Qno 3.) Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+
+// var storeName:String= "Rameez Ul Hassan"
+// var lowercase:String=storeName.toLowerCase()
+// var upperCase:String=storeName.toUpperCase()
+// var titleCase:String=storeName.toString()
+// console.log(`Lowercase = `, lowercase);
+// console.log(`Uppercase = `, upperCase);
+// console.log(`Titlecase = `, titleCase);
+
+// Qno 4.) Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+
+// Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+
+// var authorName:String = "Albert Einstein"
+// var message:String= `"A person who never made a mistake never tried anything new."`
+// console.log(`${authorName} once said, ${message}`);
+
+// Qno 5.) Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+
+// var famous_person :String = "Albert Einstein"
+// var message:String= `"A person who never made a mistake never tried anything new."`
+// console.log(`${authorName} once said, ${message}`);
+
+// Qno 6.) Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Make sure you use each character combination, "\t" and "\n", at least once. Print the name once, so the whitespace around the name is displayed. Then print the name after striping the white spaces.
+
+// var namewithWhiteSpace:String= "\t Rameez Ul Hassan \n"
+
+// console.log(`Name with Whitespace Characteristics`);
+// console.log(namewithWhiteSpace);
+
+// var removeWhiteSpace:String=namewithWhiteSpace.trim()
+// console.log(`Name remove Whitespace characteristics`);
+// console.log(removeWhiteSpace);
+
+// Qno 7.) Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+
+// var addition= 4+4;
+// console.log(`Addition = `,addition);
+
+// var subtraction=12-4;
+// console.log(`Subtraction = `,subtraction);
+
+// var multiplication=4*2;
+// console.log(`Multiplication = `,multiplication);
+
+// var division=16/2;
+// console.log(`Division = `,division);
+
+// Qno 8.) You should create four lines that look like this:
+
+// console.log(5 + 3)
+
+// Your output should simply be four lines with the number 8 appearing once on each line.
+
+// console.log(5+3);
+// console.log(12-4);
+// console.log(4*2);
+// console.log(16/2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Qno 9. Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+
+// var fNum = 13;
+// console.log(`My Favourite Number is = ${fNum}`)
+
+// Qno 10. Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+
+// Qno 11.Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+
+// var names : String[] = ["Rameez", "Usman", "Faizan"]
+
+// console.log(`First Friend ${names[0]} \n ${names[1]} \n ${names[2]}`)
+
+// Qno 12. Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+
+// var names : String[] = ["Rameez", "Usman", "Faizan"]
+
+// var Print = " is my best friend"
+
+// console.log(names[0], Print, "\n" ,names[1], Print,"\n", names[2],Print);
+
+// Qno 13. Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
+
+// var array = ["Motorcycle", "Car","Heavy Bike"];
+
+// var Print = "I would like to own a";
+
+// console.log(Print, array[0],"\n",Print , array[1])
+
+// Qno 14. Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
+
+// var invitation = ["Usman","Faizan", "Ahmad"];
+// var message = "I would like to invite for dinner";
+
+// console.log(message, invitation[0], "\n", message, invitation[1],"\n", message, invitation[2])
+
+// This program invites three people to dinner
+
+// const guests = ["Albert Einstein", "Stephen Hawking", "Marie Curie"];
+
+// for (const guest of guests) {
+//   console.log(`Dear ${guest}, I would like to invite you to dinner.`);
+// }
+
+// Qno 15. Changing Guest List: You just heard that one of your guests can’t make the dinner, so you need to send out a new set of invitations. You’ll have to think of someone else to invite.
+
+
