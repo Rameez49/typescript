dfbdc17e8854bc58e4952825857543ee0163ebd5
Rameez49 <ulhassanr59@gmail.com>
2023-09-06 20:24:21 +0500

    first

--- PIAIC/Getting Started Exercises/exercise.js
+++ PIAIC/Getting Started Exercises/exercise.js
@@ -56,29 +56,52 @@
 //     return value 
 //  })
 // Qno 13. Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
-var favTransporation = ["Motorcycle", "JHeavyBike", "Car"];
-favTransporation.map((value) => {
-    console.log(`I would like to own a ${value}`);
-});
-// Qno 10. Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
-// Qno 11.Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
-// var names : String[] = ["Rameez", "Usman", "Faizan"]
-// console.log(`First Friend ${names[0]} \n ${names[1]} \n ${names[2]}`)
-// Qno 12. Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
-// var names : String[] = ["Rameez", "Usman", "Faizan"]
-// var Print = " is my best friend"
-// console.log(names[0], Print, "\n" ,names[1], Print,"\n", names[2],Print);
-// Qno 13. Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
-// var array = ["Motorcycle", "Car","Heavy Bike"];
-// var Print = "I would like to own a";
-// console.log(Print, array[0],"\n",Print , array[1])
+// var favTransporation:String[]=["Motorcycle", "JHeavyBike","Car"]
+// favTransporation.map((value)=>{
+//     console.log(`I would like to own a ${value}`);
+// })
 // Qno 14. Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
-// var invitation = ["Usman","Faizan", "Ahmad"];
-// var message = "I would like to invite for dinner";
-// console.log(message, invitation[0], "\n", message, invitation[1],"\n", message, invitation[2])
-// This program invites three people to dinner
-// const guests = ["Albert Einstein", "Stephen Hawking", "Marie Curie"];
-// for (const guest of guests) {
-//   console.log(`Dear ${guest}, I would like to invite you to dinner.`);
-// }
+// var names:String[]=["Rameez","Ahmad","Ali","Haseeb"]
+// var message:String=", I would like to invite you in dinner"
+// names.map((value)=>{
+//     console.log(value,message);
+// })
 // Qno 15. Changing Guest List: You just heard that one of your guests can’t make the dinner, so you need to send out a new set of invitations. You’ll have to think of someone else to invite.
+// • Start with your program from Exercise 14. Add a print statement at the end of your program stating the name of the guest who can’t make it.
+// • Modify your list, replacing the name of the guest who can’t make it with the name of the new person you are inviting.
+// • Print a second set of invitation messages, one for each person who is still in your list.
+// var names:String[]=["Rameez","Ahmad","Ali","Haseeb"]
+// var message:String=", I would like to invite you in dinner"
+// names.map((value)=>{
+//     console.log(value,message);
+// })
+// var personNotCome:String="Haseeb";
+// console.log(`\nUnfoetunately ${personNotCome}, not come in dinner\n`);
+// names.pop();
+// names.push("Javaid")
+// names.map((value1)=>{
+//     console.log(value1,message);
+// })
+// Qno 16. More Guests: You just found a bigger dinner table, so now more space is available. Think of three more guests to invite to dinner.
+// • Start with your program from Exercise 15. Add a print statement to the end of your program informing people that you found a bigger dinner table.
+// • Add one new guest to the beginning of your array.
+// • Add one new guest to the middle of your array. • Use append() to add one new guest to the end of your list. • Print a new set of invitation messages, one for each person in your list.
+var names = ["Rameez", "Ahmad", "Ali", "Haseeb"];
+var message = ", I would like to invite you in dinner";
+names.map((value) => {
+    console.log(value, message);
+});
+var personNotCome = "Haseeb";
+console.log(`\nUnfoetunately ${personNotCome}, not come in dinner\n`);
+names.pop();
+names.push("Javaid");
+names.map((value1) => {
+    console.log(value1, message);
+});
+console.log(`\nNow Space is available for dinner and I want to invite some more people to dinner `);
+names.splice(0, 0, "Abdullah");
+names.splice(names.length, 0, "Faizan");
+names.splice(3, 0, "Mateen");
+names.map((value2) => {
+    console.log(value2, message);
+});
